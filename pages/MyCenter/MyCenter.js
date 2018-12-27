9efca4d7b686fc797fd83e0c49688614f5ffe862
leexin716 <517118777@qq.com>
2018-12-27 22:39:06 +0800

    1

--- pages/MyCenter/MyCenter.js
+++ pages/MyCenter/MyCenter.js
@@ -1,66 +1,52 @@
 // pages/MyCenter/MyCenter.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  toAssistant(){
+    wx.navigateTo({
+      url: '../AssistantList/AssistantList',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  toWallet(){
+    wx.navigateTo({
+      url: '../MyWallet/MyWallet',
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  toProfit() {
+    wx.navigateTo({
+      url: '../MyProfit/MyProfit',
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  toBonusDetails(){
+    wx.navigateTo({
+      url: '../BonusDetails/BonusDetails',
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  toMyCustomer(){
+    wx.navigateTo({
+      url: '../MyCustomer/MyCustomer',
+    })
+  },
+  callPhone(){
+    wx.makePhoneCall({
+      phoneNumber: '400-818181'
+    })
+  },
+  toAssistantCode(){
+    wx.navigateTo({
+      url: '../AssistantCode/AssistantCode',
+    })
+  },
+  toMyCode() {
+    wx.navigateTo({
+      url: '../MyCode/MyCode',
+    })
   }
 })
